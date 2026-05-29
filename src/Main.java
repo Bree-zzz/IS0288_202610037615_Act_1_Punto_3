ca88738f8173356e04fed64515335164da519a44
Bree-zzz <284819951+Bree-zzz@users.noreply.github.com>
2026-05-29 18:19:05 +0000

    v1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Vector;
+
+public class Main {
+public static void main(String[] args) {
+
+Transacciones logTransacciones = new Vector();
+
+logTransacciones.add("Transferencia por 10COP");
+logTransacciones.add("Transferencia por 12COP");
+logTransacciones.add("Transferencia por 15COP");
+logTransacciones.add("Transferencia por 23COP");
+logTransacciones.add("Transferencia por 80COP");
+
+
+
+
+}   
+}
---
+++ src/Transacciones.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+public class Transacciones {
+private Vector<String> vector;
+
+public Transacciones ( Vector<String>  vector) {
+    this.vector= vector;
+}
+public Vector<String> getvector() { 
+    return vector;
+} 
+public Vector<String> addStringVector(){
+    return vector;
+}
+}
